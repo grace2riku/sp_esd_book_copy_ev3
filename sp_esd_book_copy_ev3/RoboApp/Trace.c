8c23b120bec7889323bf5c88c983d159313cb65e
Koji Abe <gracehime@gmail.com>
2023-10-06 08:13:55 +0900

    Trace.c tr_runを実装し、make成功を確認した。tr_traceCourceは空実装。

--- sp_esd_book_copy_ev3/RoboApp/Trace.c
+++ sp_esd_book_copy_ev3/RoboApp/Trace.c
@@ -1,6 +1,34 @@
 #include "Trace.h"
 
+typedef enum {
+    eDiagnosis,     /* 自己診断中 */
+    eRunning,       /* 走行中 */
+} robotState_t;
+
+static robotState_t current_state;
+
+static void tr_traceCource(void);
+
+
 void tr_run(void) {
+    switch (current_state)
+    {
+    case eDiagnosis:    /* 自己診断中 */
+        /* なにもしない */
+        break;
+    case eRunning:    /* 走行中 */
+        /* 走行する */
+        tr_traceCource();
+        break;    
+    default:
+        /* 何もしない */
+        break;
+    }
+    return;
+}
+
+
+static void tr_traceCource(void) {
 
     return;
 }
