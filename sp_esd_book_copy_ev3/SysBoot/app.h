6814e1813f5d2f61f5c5dce58cfeb037ec939c69
Koji Abe <gracehime@gmail.com>
2023-10-12 08:03:11 +0900

    移植の際にベースにしたサンプルコード(sample_c4)固有の実装を削除し、本のコードを動作するために必要な初期化コードを実装

--- sp_esd_book_copy_ev3/SysBoot/app.h
+++ sp_esd_book_copy_ev3/SysBoot/app.h
@@ -82,7 +82,6 @@ extern "C" {
 #ifndef TOPPERS_MACRO_ONLY
 
 extern void main_task(intptr_t exinf);
-extern void bt_task(intptr_t exinf);
 extern void trace_task(intptr_t exinf);
 
 #endif /* TOPPERS_MACRO_ONLY */
